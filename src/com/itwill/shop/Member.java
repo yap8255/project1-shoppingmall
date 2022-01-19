76ef1bd9545e3d1faa562c8950acdf436d3fa247
xorn8 <xorn8@DESKTOP-FOBTGKT>
2022-01-19 13:40:13 +0900

    두번째커밋

--- src/com/itwill/shop/Member.java
+++ src/com/itwill/shop/Member.java
@@ -1,5 +1,16 @@
 package com.itwill.shop;
 
 public class Member {
+	private int no;
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	
 
 }
--- src/com/itwill/shop/MemberDao.java
+++ src/com/itwill/shop/MemberDao.java
@@ -1,5 +1,8 @@
 package com.itwill.shop;
 
 public class MemberDao {
+	public void insert() {
+		
+	}
 
 }
